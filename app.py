b506b082085c785d72ee61c57675eaa1a814ac4c
lin-yu-heng <92006758+lin-yu-heng@users.noreply.github.com>
2021-10-06 21:40:26 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from flask import Flask, request, abort
-
+from __future__ import print_function
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
 from linebot import (
     LineBotApi, WebhookHandler
 )
@@ -48,9 +52,47 @@ def handle_message(event):
     elif (text=="2"):
         text=list2[name]+"進度回報"
     else:
-        text=event.message.text+"\r\n\r\n\r\n"
+        text=docs()
     message = TextSendMessage(text)
     line_bot_api.reply_message(event.reply_token, message)
+    
+# If modifying these scopes, delete the file token.json.
+SCOPES = ['https://www.googleapis.com/auth/documents.readonly']
+
+# The ID of a sample document.
+DOCUMENT_ID = 'AIzaSyAuhYmNvewPz1LY2pszNMpxTjyMxI1KUik'
+
+def docs():
+    """Shows basic usage of the Docs API.
+    Prints the title of a sample document.
+    """
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    service = build('docs', 'v1', credentials=creds)
+
+    # Retrieve the documents contents from the Docs service.
+    document = service.documents().get(documentId=DOCUMENT_ID).execute()
+
+    return('The title of the document is: {}'.format(document.get('title')))
+
+
+import os.path
 import os
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
