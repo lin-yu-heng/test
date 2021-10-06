2a63d946ff60a77e9cce47c5ee10aed3de63a61c
lin-yu-heng <92006758+lin-yu-heng@users.noreply.github.com>
2021-10-06 19:46:59 +0800

    Update app.py

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     if (text=="123"):
-        text=event.userId.text
+        text=event.userId
     else:
         text=event.message.text
     message = TextSendMessage(text)
