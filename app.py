45a9938204cb000e286e9ea6b4ad3829ad7dbf0d
lin-yu-heng <92006758+lin-yu-heng@users.noreply.github.com>
2021-10-06 18:20:18 +0800

    Update app.py

--- app.py
+++ app.py
@@ -34,8 +34,10 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     message = TextSendMessage(text=event.message.text)
-    line_bot_api.reply_message(event.reply_token, message)
-
+    if (message==123):
+        line_bot_api.reply_message(event.reply_token, "成功")
+    else:
+        line_bot_api.reply_message(event.reply_token, message)
 import os
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
