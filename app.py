c9b15894fb5803fb8e92831068e9d22f302a44cd
lin-yu-heng <92006758+lin-yu-heng@users.noreply.github.com>
2021-10-06 16:06:13 +0800

    Update app.py

--- app.py
+++ app.py
@@ -33,11 +33,11 @@ def callback():
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    line_bot_api.reply_message(event.reply_token, TextSendMessage("hello"))
-#     if (test=='111'):
+    #     if (test=='111'):
 #         message = TextSendMessage("hello")
 #     else:
 #         message = TextSendMessage(text)
+    line_bot_api.reply_message(event.reply_token, TextSendMessage("hello"))
     message = TextSendMessage("hello")
     line_bot_api.reply_message(event.reply_token, message)
 import os
